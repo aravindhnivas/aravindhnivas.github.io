8355b4cc15638202ecef4cc3cad93b3106fbdb85
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-07-30 14:38:40 +0200

    integrate unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://aravindhnivas.github.io',
+    integrations: [
+        UnoCSS(),
+    ],
 });
